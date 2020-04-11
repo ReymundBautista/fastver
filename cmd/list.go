f6e521a774d8c21b03d4f4a3399fe5c8eab3d53e
Reymund Bautista <reymund.bautista@gmail.com>
2020-04-11 03:33:16 -0400

    chore(services list): update cmd descriptions

--- cmd/list.go
+++ cmd/list.go
@@ -13,19 +13,15 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List Fastly services",
+	Long: `Lists all Fastly services along with their ID that is needed for
+	other commands`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if getenv.Exists(apiTokenEnvName) {
 			client := newFastlyClient(apiTokenEnvName)
 			services, _ := listServices(client)
 			for _, service := range services {
-				fmt.Println(service.Name)
+				fmt.Println(service.Name + " ID: " + service.ID)
 			}
 
 		} else {
@@ -48,7 +44,7 @@ func init() {
 	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// Interface that has the same GetCurrentUser() signature from fastly.Client
+// Interface that has the same ListServices() signature from fastly.Client
 type fastlyClientSerivces interface {
 	ListServices(i *fastly.ListServicesInput) ([]*fastly.Service, error)
 }
